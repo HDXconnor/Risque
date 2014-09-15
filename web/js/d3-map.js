2caf9e1e88fd722c699d0057e08bbd0d4745cd22
Anthony <anthonycrowcroft@hotmail.com>
2014-09-15 19:29:14 +1200

    because

--- web/js/d3-map.js
+++ web/js/d3-map.js
@@ -4,10 +4,16 @@
 
 //Make an SVG Container
 app.controller("MapController", ["$rootScope", '$http', function($rootScope, $http) {
-    var mapData = JSON.parse("mapPaths.json");
+    var mapData = JSON.parse("../json/mapPaths.json");
     var svgMap = d3.select("body").append("svgMap")
                                   .attr("width", 900)
                                   .attr("height", 900);
-
-
+    angular.forEach(mapData.Map, function(index) {
+        svgMap.append("path")
+              .attr("id", index.id)
+              .attr("class", "country-shape")
+              .attr("d", lineFunction(index.d))
+              .attr("stroke", index.stroke)
+              .attr("fill", index.fill)
+    };
 }]);
