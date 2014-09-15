1a8a38d7f6e33d1885481df2cf0c86cc3a86d8ba
firao <fir4g4@gmail.com>
2014-09-15 21:48:33 +1200

    renamed html folder, added angular map

--- web/js/d3-map.js
+++ web/js/d3-map.js
@@ -1,18 +1,18 @@
-
-
-app.controller("MapController", ["$rootScope", '$http', function($rootScope, $http) {
-    var mapData = JSON.parse("../json/mapPaths.json");
-    //Make an SVG Container
-    var svgMap = d3.select("body").append("svgMap")
-                                  .attr("width", 900)
-                                  .attr("height", 900);
-    //Add Countries to SVG Container
-    angular.forEach(mapData.Map, function(index) {
-        svgMap.append("path")
-              .attr("id", index.id)
-              .attr("class", "country-shape")
-              .attr("d", lineFunction(index.d))
-              .attr("stroke", index.stroke)
-              .attr("fill", index.fill)
-    };
-}]);
+//
+//
+//app.controller("MapController", ["$rootScope", '$http', function($rootScope, $http) {
+//    var mapData = JSON.parse("../json/mapPaths.json");
+//    //Make an SVG Container
+//    var svgMap = d3.select("body").append("svgMap")
+//                                  .attr("width", 900)
+//                                  .attr("height", 900);
+//    //Add Countries to SVG Container
+//    angular.forEach(mapData.Map, function(index) {
+//        svgMap.append("path")
+//              .attr("id", index.id)
+//              .attr("class", "country-shape")
+//              .attr("d", lineFunction(index.d))
+//              .attr("stroke", index.stroke)
+//              .attr("fill", index.fill)
+//    };
+//}]);
