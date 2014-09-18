bf36716f75e6d23d7c81b9a3aa4121da980a5f14
JMdeKlerk <shindogo@gmail.com>
2014-09-18 20:53:20 +1200

    Command updates and such

--- src/java/game/server/GameServlet.java
+++ src/java/game/server/GameServlet.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package game.server;
 
 import game.logic.Command;
@@ -26,19 +25,11 @@ import org.json.JSONObject;
  * @author Simeon
  */
 public class GameServlet extends HttpServlet {
-    
+
     //Game game = new Game(new PlayerList("Awaiting player...","Awaiting player...","Awaiting player...","Awaiting player...","Awaiting player...","Awaiting player..."));
     Game game = new Game(new PlayerList());
     boolean useSSE = false;
 
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         tempJoinGame(request);
@@ -48,60 +39,49 @@ public class GameServlet extends HttpServlet {
             try (PrintWriter out = response.getWriter()) {
                 JSONObject json = game.getGameJSON();
                 System.out.println("GET data sending:   " + json);
-                if (game.updated()) out.write("event: gamestate\ndata: " + json + "\n\n"); // SSE requires a blank line: \n\n
-            } catch (JSONException e) {}
-            
-        // keeping the old method around for testing purposes...
+                if (game.updated()) {
+                    out.write("event: gamestate\ndata: " + json + "\n\n"); // SSE requires a blank line: \n\n
+                }
+            } catch (JSONException e) {
+            }
+
+            // keeping the old method around for testing purposes...
         } else {
             response.setContentType("application/json");
             try (PrintWriter out = response.getWriter()) {
                 JSONObject json = game.getGameJSON();
                 out.print(json);
                 System.out.println("GET data sending:   " + json);
-            } catch (JSONException e) {}
+            } catch (JSONException e) {
+            }
         }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            
             JSONObject clicked = new JSONObject(request.getParameter("clicked"));
             System.out.println("POST data received: " + clicked);
             Command.parseInput(clicked, game);
         } catch (JSONException e) {
-        
-        }
-        
-        
-    }
-    
-    public void tempJoinGame(HttpServletRequest request) {
-        
 
+        }
+    }
+
+    public void tempJoinGame(HttpServletRequest request) {
         try {
             String name = request.getCookies()[0].getValue();
             HashMap<Integer, Player> players = game.getPlayers().getPlayers();
-            for (Player p:players.values()) {
-                if (p.getName().equals(name)) return;
+            for (Player p : players.values()) {
+                if (p.getName().equals(name)) {
+                    return;
+                }
             }
             game.getPlayers().joinGame(new Player(name, 20));
-        } catch (PlayerException | NullPointerException ex) {}
+        } catch (PlayerException | NullPointerException ex) {
+        }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Risque Game Servlet.";
