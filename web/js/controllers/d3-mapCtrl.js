55f97bffdd9214aa14c901e10d57b6112ee28ce0
firao <fir4g4@gmail.com>
2014-10-03 16:47:28 +1300

    moved adding event listeners into get request success function

--- web/js/controllers/d3-mapCtrl.js
+++ web/js/controllers/d3-mapCtrl.js
@@ -1,123 +1,125 @@
 'use strict';
 //Not yet implemented into html file
 angular.module('gameApp')
-    .controller("MapController", ["$rootScope", '$http', function ($rootScope, $http) {
+        .controller("MapController", ["$rootScope", '$http', function ($rootScope, $http) {
 
-        //Make an SVG Container
-        $rootScope.svgMap = d3.select("#svgMap").append("svg")
-            .attr("width", 900)
-            .attr("height", 900);
-
-
-        $rootScope.mapList = {};
-        $http.get('json/mapPaths.json').success(function (mapData){
+                //Make an SVG Container
+                $rootScope.svgMap = d3.select("#svgMap").append("svg")
+                        .attr("width", 900)
+                        .attr("height", 900);
 
 
-            //Add Countries to SVG Container
-            angular.forEach(mapData.Map, function(index) {
-                $rootScope.mapList[String(index.id)] = $rootScope.svgMap.append("path")
-                    .attr("id", index.id)
-                    .attr("d", index.d)
-                    .attr("stroke", index.stroke)
-                    .attr("fill", index.fill);
-            });
+                $rootScope.mapList = {};
+                $http.get('json/mapPaths.json').success(function (mapData) {
 
-        });
-
-        console.log($rootScope.mapList);
-
-        angular.forEach($rootScope.mapList, function (index) {
-
-            index[0].addEventListener("mouseover", function () {
-                $rootScope.thisCountryID = index.node.id;
-                angular.forEach($rootScope.board, function (index) {
-                    if ($rootScope.thisCountryID === index.CountryID) {
-                        $rootScope.countryName = index.CountryName;
-                        $rootScope.owner = index.Owner;
-                        $rootScope.troops = index.Troops;
-                    }
-                    angular.forEach($rootScope.players, function (player) {
-                        if ($rootScope.owner === player.PlayerOrder) {
-                            $rootScope.playerName = player.DisplayName;
-                        }
+                    //Add Countries to SVG Container
+                    angular.forEach(mapData.Map, function (index) {
+                        $rootScope.mapList[String(index.id)] = $rootScope.svgMap.append("path")
+                                .attr("id", index.id)
+                                .attr("d", index.d)
+                                .attr("stroke", index.stroke)
+                                .attr("fill", index.fill);
                     });
-                });
-                angular.forEach($rootScope.board, function (index){
-                    if ($rootScope.thisCountryID === index.CountryID) {
-                        if($rootScope.currentPlayer!==index.Owner){
-                            $rootScope.defendCountryName = index.CountryName;
-                            $rootScope.defendOwner = index.Owner;
-                            $rootScope.defendTroops = index.Troops;
-                        }else{
-                            $rootScope.attackCountryName = index.CountryName;
-                            $rootScope.attackOwner = index.Owner;
-                            $rootScope.attackTroops = index.Troops;
-                        }}
-                    angular.forEach($rootScope.players, function (player) {
-                        if ($rootScope.attackOwner === player.PlayerOrder) {
-                            $rootScope.attackOwner = player.DisplayName;
-                        };
-                        if($rootScope.defendOwner === player.PlayerOrder){
-                            $rootScope.defendOwner = player.DisplayName;
-                        }
 
-                    });
-                });
 
-            }, true);
+                    angular.forEach($rootScope.mapList, function (index) {
 
-            index[0].addEventListener("mouseout", function () {
-            }, true);
-
-            index[0].addEventListener("click", function () {
-                $rootScope.thisCountryID = index.node.id;
-
-                if ($rootScope.currentPlayer === $rootScope.thisUserNumber) {
-                    index.animate(defaultCountry, animationSpeed);
-                    if ($rootScope.phase === "Setup") {
-                        var send = JSON.stringify({Command: "Setup", Data: {CountryClicked: $rootScope.thisCountryID, CurrentPlayer: $rootScope.currentPlayer}});
-                        postData(send);
-                    }
-                    if ($rootScope.phase === "Deploy") {
-                        var send = JSON.stringify({Command: "Deploy", Data: {CountryClicked: $rootScope.thisCountryID, CurrentPlayer: $rootScope.currentPlayer}});
-                        postData(send);
-                    }
-                    if ($rootScope.phase === "Attack") {
-                        angular.forEach($rootScope.board, function (index){
-                            if ($rootScope.thisCountryID === index.CountryID) {
-                                if($rootScope.currentPlayer === index.Owner){
-                                    $rootScope.attackCountryID = $rootScope.thisCountryID;
-                                    console.log("attack");
+                        index[0][0].addEventListener("mouseover", function () {
+                            $rootScope.thisCountryID = index.node.id;
+                            
+                            angular.forEach($rootScope.board, function (index) {
+                                if ($rootScope.thisCountryID === index.CountryID) {
+                                    $rootScope.countryName = index.CountryName;
+                                    $rootScope.owner = index.Owner;
+                                    $rootScope.troops = index.Troops;
                                 }
-                                if($rootScope.currentPlayer !== index.Owner){
-                                    $rootScope.defendCountryID = $rootScope.thisCountryID;
-                                    console.log("defend");
+                                angular.forEach($rootScope.players, function (player) {
+                                    if ($rootScope.owner === player.PlayerOrder) {
+                                        $rootScope.playerName = player.DisplayName;
+                                    }
+                                });
+                            });
+                            
+                            angular.forEach($rootScope.board, function (index) {
+                                if ($rootScope.thisCountryID === index.CountryID) {
+                                    if ($rootScope.currentPlayer !== index.Owner) {
+                                        $rootScope.defendCountryName = index.CountryName;
+                                        $rootScope.defendOwner = index.Owner;
+                                        $rootScope.defendTroops = index.Troops;
+                                    } else {
+                                        $rootScope.attackCountryName = index.CountryName;
+                                        $rootScope.attackOwner = index.Owner;
+                                        $rootScope.attackTroops = index.Troops;
+                                    }
                                 }
+                                
+                                angular.forEach($rootScope.players, function (player) {
+                                    if ($rootScope.attackOwner === player.PlayerOrder) {
+                                        $rootScope.attackOwner = player.DisplayName;
+                                    }
+                                    ;
+                                    if ($rootScope.defendOwner === player.PlayerOrder) {
+                                        $rootScope.defendOwner = player.DisplayName;
+                                    }
+
+                                });
+                            });
+
+                        }, true);
+
+                        index[0][0].addEventListener("mouseout", function () {
+                        }, true);
+
+                        index[0][0].addEventListener("click", function () {
+                            $rootScope.thisCountryID = index.node.id;
+
+                            if ($rootScope.currentPlayer === $rootScope.thisUserNumber) {
+                                index.animate(defaultCountry, animationSpeed);
+                                if ($rootScope.phase === "Setup") {
+                                    var send = JSON.stringify({Command: "Setup", Data: {CountryClicked: $rootScope.thisCountryID, CurrentPlayer: $rootScope.currentPlayer}});
+                                    postData(send);
+                                }
+                                if ($rootScope.phase === "Deploy") {
+                                    var send = JSON.stringify({Command: "Deploy", Data: {CountryClicked: $rootScope.thisCountryID, CurrentPlayer: $rootScope.currentPlayer}});
+                                    postData(send);
+                                }
+                                if ($rootScope.phase === "Attack") {
+                                    angular.forEach($rootScope.board, function (index) {
+                                        if ($rootScope.thisCountryID === index.CountryID) {
+                                            if ($rootScope.currentPlayer === index.Owner) {
+                                                $rootScope.attackCountryID = $rootScope.thisCountryID;
+                                                console.log("attack");
+                                            }
+                                            if ($rootScope.currentPlayer !== index.Owner) {
+                                                $rootScope.defendCountryID = $rootScope.thisCountryID;
+                                                console.log("defend");
+                                            }
+                                        }
+                                    });
+
+
+                                }
+                                if ($rootScope.phase === "Move") {
+                                    var send = JSON.stringify({Command: "Move", Data: {SourceCountry: $rootScope.prevCountryID, CountryClicked: $rootScope.thisCountryID, CurrentPlayer: $rootScope.currentPlayer}});
+                                    postData(send);
+                                }
+                                $rootScope.prevCountryID = index.node.id;
+
                             }
-                        });
+                            else {
+                                console.log("Not your turn");
+                            }
+                        }, true);
+                    });
+                });
 
-
-                    }
-                    if ($rootScope.phase === "Move") {
-                        var send = JSON.stringify({Command: "Move", Data: {SourceCountry: $rootScope.prevCountryID, CountryClicked: $rootScope.thisCountryID, CurrentPlayer: $rootScope.currentPlayer}});
-                        postData(send);
-                    }
-                    $rootScope.prevCountryID = index.node.id;
-
+                function postData(data) {
+                    $http({
+                        method: 'POST',
+                        url: 'GameServlet',
+                        headers: {'Content-Type': 'application/json'},
+                        data: data
+                    }).error();
                 }
-                else {
-                    console.log("Not your turn");
-                }
-            }, true);
-        });
 
-        function postData(data) {
-            $http({
-                method: 'POST',
-                url: 'GameServlet',
-                headers: {'Content-Type': 'application/json'},
-                data: data
-            }).error();
-        }
-
-        }]);
+            }]);
