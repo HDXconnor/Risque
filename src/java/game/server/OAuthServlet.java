e891ee74a8d085f7c3061ab7cf70b138a3348f4c
JMdeKlerk <shindogo@gmail.com>
2014-10-07 21:43:29 +1300

    Moving from laptop to desktop, pls ignore

--- src/java/game/server/OAuthServlet.java
+++ src/java/game/server/OAuthServlet.java
@@ -8,45 +8,45 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.scribe.builder.ServiceBuilder;
 import org.scribe.builder.api.FacebookApi;
-import org.scribe.model.OAuthRequest;
-import org.scribe.model.Response;
+import org.scribe.builder.api.GoogleApi;
 import org.scribe.model.Token;
-import org.scribe.model.Verb;
-import org.scribe.model.Verifier;
 import org.scribe.oauth.OAuthService;
 
 public class OAuthServlet extends HttpServlet {
 
     private static final Token EMPTY_TOKEN = null;
-    private static String apiKey = "766984246696208";
-    private static String apiSecret = "a92c4944d4c9a4c3c8247c4514cdbd46";
-    OAuthService service;
+    private static String callback = "http://111.69.101.129:8084/Risque/OAuthServlet";
+    private static String facebookKey = "766984246696208";
+    private static String facebookSecret = "a92c4944d4c9a4c3c8247c4514cdbd46";
+    private static String googleKey = "anonymous";
+    private static String googleSecret = "anonymous";
+    private static final String googleScope = "https://www.googleapis.com/auth/plus.login";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        Verifier verifier = new Verifier(request.getParameter("code"));
-        Token accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
-//        PrintWriter out = response.getWriter();
-//        out.println(request.getParameter("code"));
-
-        // Testing our access
-        OAuthRequest authRequest = new OAuthRequest(Verb.GET, "https://graph.facebook.com/me");
-        service.signRequest(accessToken, authRequest);
-        Response authResponse = authRequest.send();
-        PrintWriter out = response.getWriter();
-        out.println(authResponse.getCode());
-        out.println(authResponse.getBody());
+        if (request.getParameter("service").equals("facebook")) {
+            OAuthService service = new ServiceBuilder().provider(FacebookApi.class).apiKey(facebookKey).apiSecret(facebookSecret).callback(callback).build();
+            String state = request.getParameter("id");
+            String authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN) + "&state=" + state;
+            response.setContentType("text/html");
+            response.setStatus(response.SC_MOVED_TEMPORARILY);
+            response.setHeader("Location", authorizationUrl);
+        } else if (request.getParameter("service").equals("google")) {
+            OAuthService service = new ServiceBuilder().provider(GoogleApi.class).apiKey(googleKey).apiSecret(googleSecret).scope(googleScope).callback(callback).build();
+            Token requestToken = service.getRequestToken();
+            String state = request.getParameter("id");
+            String authorizationUrl = "https://www.google.com/accounts/OAuthAuthorizeToken?oauth_token=" + requestToken.getToken() + "&state=" + state;
+            response.setContentType("text/html");
+            response.setStatus(response.SC_MOVED_TEMPORARILY);
+            response.setHeader("Location", authorizationUrl);
+        } else {
+            PrintWriter out = response.getWriter();
+            out.println("Invalid service");
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        service = new ServiceBuilder().provider(FacebookApi.class).apiKey(apiKey).apiSecret(apiSecret).callback("http://111.69.101.129:8084/Risque/OAuthServlet").build();
-        String authorizationUrl = service.getAuthorizationUrl(EMPTY_TOKEN); // Send the user here clientside
-//        PrintWriter out = response.getWriter();
-//        out.println(authorizationUrl);
-        response.setContentType("text/html");
-        response.setStatus(response.SC_MOVED_TEMPORARILY);
-        response.setHeader("Location", authorizationUrl);
+
     }
 }
--- web/js/controllers/loginCtrl.js
+++ web/js/controllers/loginCtrl.js
@@ -2,49 +2,74 @@
 
 angular.module('gameApp')
 
-    .controller('LoginController', ['$rootScope', '$cookieStore', '$http', function ($rootScope, $cookieStore, $http) {
-        this.setUser = function () {
-            var loginName = document.getElementById("login-textbox").value;
-            var joinData = JSON.stringify({Command: "Join", Data: {CurrentPlayer: loginName}});
-            postData(joinData);
-            writeCookie("Username", loginName);
-            console.log("Logged in as: "+loginName);
-            //$cookieStore.put("Username", $rootScope.username);
-        };
+        .controller('LoginController', ['$rootScope', '$cookieStore', '$http', function ($rootScope, $cookieStore, $http) {
+                this.setUser = function () {
+                    var loginName = document.getElementById("login-textbox").value;
+                    var joinData = JSON.stringify({Command: "Join", Data: {CurrentPlayer: loginName}});
+                    postData(joinData);
+                    writeCookie("Username", loginName);
+                    console.log("Logged in as: " + loginName);
+                    //$cookieStore.put("Username", $rootScope.username);
+                };
 
-        this.loginVis = function () {
-            return(document.cookie.indexOf("Username") >= 0);
-        };
+                this.facebook = function () {
+                    var id = makeID();
+                    var url = 'OAuthServlet?service=facebook&id=' + id;
+                    console.log(url);
+                    $http({method: 'GET', url: url, headers: {'Content-Type': 'text/plain'}}).error();
+                };
 
-        this.delCookie = function () {
-            var cookie = readCookie();
-            console.log("deleting cookie: " +cookie);
-            for (var index in cookie) {
-                var name = cookie[index];
-            }
-            name = name.replace('Username=', '');
-            var quitData = JSON.stringify({Command: "Quit", Data: {CurrentPlayer: $rootScope.thisPlayer}});
-            document.cookie = "Username=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-            postData(quitData);
-            $rootScope.$apply();
-        };
+                this.google = function () {
+                    var id = makeID();
+                    var url = 'OAuthServlet?service=google&id=' + id;
+                    console.log(url);
+                    $http({method: 'GET', url: url, headers: {'Content-Type': 'application/json'}, data: 1}).error();
+                };
 
-        function readCookie() {
-            var x = document.cookie;
-            return x.split("; ");
-        }
+                this.loginVis = function () {
+                    return(document.cookie.indexOf("Username") >= 0);
+                };
 
-        function postData(data) {
-            $http({
-                method: 'POST',
-                url: 'GameServlet',
-                headers: {'Content-Type': 'application/json'},
-                data: data
-            }).error();
-        }
+                this.delCookie = function () {
+                    var cookie = readCookie();
+                    console.log("deleting cookie: " + cookie);
+                    for (var index in cookie) {
+                        var name = cookie[index];
+                    }
+                    name = name.replace('Username=', '');
+                    var quitData = JSON.stringify({Command: "Quit", Data: {CurrentPlayer: $rootScope.thisPlayer}});
+                    document.cookie = "Username=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+                    postData(quitData);
+                    $rootScope.$apply();
+                };
 
-        function writeCookie(key, value) {
-            document.cookie = key + "=" + value + ";";
-        }
+                function readCookie() {
+                    var x = document.cookie;
+                    return x.split("; ");
+                }
 
-    }]);
+                function postData(data) {
+                    $http({
+                        method: 'POST',
+                        url: 'GameServlet',
+                        headers: {'Content-Type': 'application/json'},
+                        data: data
+                    }).error();
+                }
+
+                function writeCookie(key, value) {
+                    document.cookie = key + "=" + value + ";";
+                }
+
+                function makeID()
+                {
+                    var text = "";
+                    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+                    for (var i = 0; i < 5; i++)
+                        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+                    return text;
+                }
+
+            }]);
